5fcb75d97fa7b283b5322c56b6700a9d7994adff
tr4nquility <tr4nquility_s@yahoo.com>
2016-06-06 18:33:47 +0700

    Update sqlConnect.js

--- sqlConnect.js
+++ sqlConnect.js
@@ -1,34 +1,42 @@
-var sql = require('mssql'); 
+var sql = require('mssql');
 
-function SqlConnect(config){
-    configuration = {
+function SqlConnect(config) {
+    var configuration = {
         user: config.user,
         password: config.password,
         server: config.server,
         port: config.port,
         database: config.database,
         options: {
-            encrypt: config.encrypt
+            encrypt: config.options.encrypt
         }
     };
+    connection = new sql.Connection(configuration);
 }
 
-SqlConnect.prototype.query = function getQueryResult(sqlString){
-    
-    return new Promise(function(fulfill, reject){
-       var connection = new sql.Connection(configuration, function(err){
-           if (err) {
-               reject(err);
-           } 
-           else {
-               var request = new sql.Request(connection);
-               request.query(sqlString).then(result => fulfill(result)).catch( error => reject(error));
-           }  
-       });
-    });
+SqlConnect.prototype.query = function getQueryResult(sqlString) {
+    return new Promise(function (fulfill, reject) {
+        if (connection) {
+            // try to connect using specified connection
+            connection.connect().then(function () {
+                // create a new request
+                var request = new sql.Request(connection);
+                request.query(sqlString, function (err, recordset) {
+                    if (err) reject({
+                        code: err.code,
+                        message: err.message
+                    });
+                    else fulfill({ query: sqlString, result: recordset });
+                });
+            }).catch(err => reject({
+                code: err.code,
+                message: err.message
+            }));
+        }
+        else
+            reject('SQL Connection is not available.');
+    })
 }
-
-
 module.exports = SqlConnect;
 
 
