1c69e7b091e3d29a38f3b6a919d01980f895539b
tr4nquility <tr4nquility_s@yahoo.com>
2016-06-07 09:19:58 +0700

    Initial sample structure for SELECT queries (#3)
    * Add files via upload
    
    * Delete pull.txt
    
    * Update index.js
    
    * Update package.json
    
    * Update README.md
    
    * Update README.md
    
    * Update sqlConnect.js
    
    * Update sqlConnect.js
    
    * Update README.md
    
    * Update sqlConnect.js
    
    * Update package.json

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # bateeq-legacy-db-module
-Module to retrieve data from Bateeq legacy database
+Module to retrieve data from Bateeq legacy database.
+
+# Usage example :
+```javascript
+var db = require('bateeq-legacy-db-module');
+var db_config = {
+    user: '<username>',  
+    password: '<password>',
+    server: '<sql_server>', // You can use 'localhost\\instance' to connect to named instance
+    port: <sql_port>,
+    database: '<sql_db_name>',
+    debug: false,
+    options: {
+        encrypt: false // Set this to <true> if you're on Windows Azure
+    }
+}
+var conn = new db.SqlConnection(db_config);
+conn.query('SELECT TOP 1 * FROM <your_table_name>').then(result => console.log(result)).catch(error => console.log(error));
+```
---
+++ sqlConnect.js
@@ -0,0 +1,46 @@
+var sql = require('mssql');
+
+function SqlConnect(config) {
+    configuration = {
+        user: config.user,
+        password: config.password,
+        server: config.server,
+        port: config.port,
+        database: config.database,
+        options: {
+            encrypt: config.options.encrypt
+        }
+    };
+}
+
+SqlConnect.prototype.query = function getQueryResult(sqlString) {
+    var connection = new sql.Connection(configuration);
+    return new Promise(function (fulfill, reject) {
+        if (connection) {
+            // try to connect using specified connection
+            connection.connect().then(function () {
+                // create a new request
+                var request = new sql.Request(connection);
+                request.query(sqlString, function (err, recordset) {
+                    if (err)
+                        reject({
+                            code: err.code,
+                            message: err.message
+                        });
+                    else
+                        fulfill({
+                            query: sqlString,
+                            result: recordset
+                        });
+                });
+            }).catch(err => reject({
+                code: err.code,
+                message: err.message
+            }));
+        }
+        else
+            reject('SQL Connection is not available.');
+    })
+}
+
+module.exports = SqlConnect;
