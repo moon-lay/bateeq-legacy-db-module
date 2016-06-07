2f5ea84df1a32646a292d9df95a953040e31e936
tr4nquility <tr4nquility_s@yahoo.com>
2016-06-07 15:59:16 +0700

    Update sqlConnect.js

--- sqlConnect.js
+++ sqlConnect.js
@@ -43,4 +43,59 @@ SqlConnect.prototype.query = function getQueryResult(sqlString) {
     })
 }
 
+SqlConnect.prototype.execSP = function getResult(options) {
+    var connection = new sql.Connection(configuration);
+    return new Promise(function (fulfill, reject) {
+        if (connection) {
+            connection.connect().then(function () {
+                if (!options.hasOwnProperty('sp_name'))
+                    reject('sp_name is missing.');
+
+                // create a new request
+                var request = new sql.Request(connection);
+                request.multiple = false;
+                if (options.hasOwnProperty('params')) {
+                    Object.keys(options.params).forEach(function (key) {
+                        var value = options.params[key];
+                        switch (Object.prototype.toString.call(value)) {
+                            case "[object String]":
+                                request.input(key, sql.NVarChar, value);
+                                break;
+                            case "[object Number]":
+                                request.input(key, sql.Int, value);
+                                break;
+                            case "[object Date]":
+                                request.input(key, sql.DateTime, value);
+                                break;
+                            default:
+                                request.input(key, sql.NVarChar, value);
+                                break;
+                        }
+                    })
+                }
+
+                request.execute(options.sp_name, function (err, recordset, returnValue, affected) {
+                    if (err)
+                        reject({
+                            code: err.code,
+                            message: err.message
+                        })
+                    else {
+                        fulfill({
+                            retVal: returnValue,
+                            affected: affected,
+                            data: recordset[0]
+                        })
+                    }
+                });
+            }).catch(err => reject({
+                code: err.code,
+                message: err.message
+            }));
+        }
+        else
+            reject('SQL Connection is not available');
+    });
+}
+
 module.exports = SqlConnect;
